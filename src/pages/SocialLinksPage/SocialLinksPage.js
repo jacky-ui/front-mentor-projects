0fb554baeb8da38bd6498f4ff458f199508bc140
jacky-ui <jacky.du.004@gmail.com>
2024-11-01 12:34:35 -0400

    import useSetTitle into social links profile

--- src/pages/SocialLinksPage/SocialLinksPage.js
+++ src/pages/SocialLinksPage/SocialLinksPage.js
@@ -1,6 +1,8 @@
+import useSetTitle from '../../utils/SetTitle';
 import './SocialLinksPage.scss';
 
 function SocialLinksPage() {
+    useSetTitle('Front-End Mentor | Social Links Profile');
     return(
         <>
             <h1>Hello World</h1>
