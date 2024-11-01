d9408c14cea4dac71752b11eff9ea09b63795950
jacky-ui <jacky.du.004@gmail.com>
2024-11-01 13:59:27 -0400

    basic html skeleton created

--- src/pages/SocialLinksPage/SocialLinksPage.js
+++ src/pages/SocialLinksPage/SocialLinksPage.js
@@ -1,12 +1,22 @@
 import useSetTitle from '../../utils/SetTitle';
+import avatar from '../../assets/images/avatar-jessica.jpeg';
 import './SocialLinksPage.scss';
 
 function SocialLinksPage() {
     useSetTitle('Front-End Mentor | Social Links Profile');
     return(
-        <>
-            <h1>Hello World</h1>
-        </>
+        <article className='socialCard'>
+            <img src={avatar} alt='___ profile avatar'/>
+            <h1>Jessica Randall</h1>
+            <p>London, United Kingdom</p>
+            <div className='socialCard__links'>
+                <a href='www.google.com'>Github</a>
+                <a href='www.google.com'>LinkedIn</a>
+                <a href='www.google.com'>Frontend Mentor</a>
+                <a href='www.google.com'>Twitter</a>
+                <a href='www.google.com'>Instagram</a>
+            </div>
+        </article>
     )
 };
 
