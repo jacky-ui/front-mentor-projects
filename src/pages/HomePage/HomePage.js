4606f710a6733e8cd926703873d49329df6b52b2
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 15:30:28 -0500

    looks good on desktop

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -13,6 +13,23 @@ function HomePage() {
                     <h2>NEWBIE</h2>
                     <span className='homeContent__underline'></span>
                     <div className='homeContent__projects'>
+                        <Link className='projects' to='/social-links'>
+                            <img src={socialLinks} alt=''/>
+                            <figcaption className='projects__caption'>
+                                <h3>Social Links Profile</h3>
+                                <p>Challenge to build social link-sharing profile</p>
+                                <div className='projects__tools'>
+                                    <p>#react</p>
+                                    <p>#scss</p>
+                                </div>
+                            </figcaption>
+                        </Link>
+                        <Link to='/social-links'>
+                            <img src={socialLinks} alt=''/>
+                        </Link>
+                        <Link to='/social-links'>
+                            <img src={socialLinks} alt=''/>
+                        </Link>
                         <Link to='/social-links'>
                             <img src={socialLinks} alt=''/>
                         </Link>
