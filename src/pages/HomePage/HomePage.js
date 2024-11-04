6f6db51c0586572fd97f9ea85c65155f200d9722
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 14:39:52 -0500

    create basic home outline and add animation to h2

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,20 @@
+import useSetBodyClass from '../../utils/SetBodyClass';
 import './HomePage.scss';
 
 function HomePage() {
+    useSetBodyClass('home')
     return(
         <>
             <h1>Home</h1>
+            <section className='homeContent'>
+                <article>
+                    <h2>NEWBIE</h2>
+                    <span className='homeContent__underline'></span>
+                    <div>
+
+                    </div>
+                </article>
+            </section>
         </>
     )
 }
