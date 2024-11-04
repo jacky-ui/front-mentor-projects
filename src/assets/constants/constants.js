ad5e2fdc3bcd091664e49d3f217f084d2e9e601e
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 11:40:52 -0500

    create constant file

---
+++ src/assets/constants/constants.js
@@ -0,0 +1,31 @@
+import scAvatar from '../images/avatar-jessica.jpeg';
+
+const socialProfileContent = [
+    {
+        name: "Jessica Randall",
+        location: "London, United Kingdom",
+        description: "Front-end developer and avid reader.",
+        links: [
+            {
+                anchorName: "Github",
+                anchorRef: "https://github.com/",
+            },
+            {
+                anchorName: "Frontend Mentor",
+                anchorRef: "https://www.frontendmentor.io/",
+            },
+            {
+                anchorName: "LinkedIn",
+                anchorRef: "https://www.linkedin.com/",
+            },
+            {
+                anchorName: "Twitter",
+                anchorRef: "https://twitter.com/",
+            },
+            {
+                anchorName: "Instagram",
+                anchorRef: "https://www.instagram.com/",
+            },
+        ]
+    }
+];
