84462a59f0c96c38e44849cf126190ce0b216376
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 18:37:31 -0500

    import style for advicecard

---
+++ src/components/AdviceCard/AdviceCard.js
@@ -0,0 +1,3 @@
+import './AdviceCard.scss';
+import desktopDivider from '../../assets/images/pattern-divider-desktop.svg';
+import mobileDivider from '../../assets/images/pattern-divider-mobile.svg';
