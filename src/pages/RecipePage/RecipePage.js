d77c543aa636230fd659ed62f0153342d3ef12ea
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 23:53:08 -0500

    ingredients section looks good on mobile and desktop

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -16,9 +16,19 @@ function RecipePage() {
             <section className='prepContain'>
                 <h2>Preparation time</h2>
                 <ul className='prepContain__lists'>
-                    <li><span className='prepContain__lists--spacing'><span>Total:</span> Approximately 10 minutes</span></li>
-                    <li><span className='prepContain__lists--spacing'><span>Preparation:</span> 5 minutes</span></li>
-                    <li><span className='prepContain__lists--spacing'><span>Cooking:</span> 5 minutes</span></li>
+                    <li><span className='list--spacing'><span>Total:</span> Approximately 10 minutes</span></li>
+                    <li><span className='list--spacing'><span>Preparation:</span> 5 minutes</span></li>
+                    <li><span className='list--spacing'><span>Cooking:</span> 5 minutes</span></li>
+                </ul>
+            </section>
+            <section className='ingredients'>
+                <h2>Ingredients</h2>
+                <ul>
+                    <li><span className='list--spacing'>2-3 large eggs</span></li>
+                    <li><span className='list--spacing'>Salt, to taste</span></li>
+                    <li><span className='list--spacing'>Pepper, to taste</span></li>
+                    <li><span className='list--spacing'>1 tablespoon of butter or oil</span></li>
+                    <li><span className='list--spacing'>Optional fillings: cheese, diced vegetables, cooked meats, herbs</span></li>
                 </ul>
             </section>
         </section>
