51df511b99054eb67916dd2f558795f9cb9fa8c4
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 21:38:32 -0500

    some progress

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -13,11 +13,13 @@ function RecipePage() {
                 <h1>Simple Omelette Recipe</h1>
                 <p className='heading__description'>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
             </section>
-            <section className='preContain'>
+            <section className='prepContain'>
                 <h2>Preparation time</h2>
-                <ol>
-
-                </ol>
+                <ul className='prepContain__lists'>
+                    <li><span>Total:</span> Approximately 10 minutes</li>
+                    <li><span>Preparation:</span> 5 minutes</li>
+                    <li><span>Cooking:</span> 5 minutes</li>
+                </ul>
             </section>
         </section>
     )
