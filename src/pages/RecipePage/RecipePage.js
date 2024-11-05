1fb0577fd7813bd27db6a60a988bab19bcaefb49
jacky-ui <jacky.du.004@gmail.com>
2024-11-04 20:40:52 -0500

    create new route for recipe

---
+++ src/pages/RecipePage/RecipePage.js
@@ -0,0 +1,12 @@
+import useSetBodyClass from '../../utils/SetBodyClass';
+import './RecipePage.scss';
+
+function RecipePage() {
+    useSetBodyClass("rpBody");
+    return(
+        <>
+        </>
+    )
+};
+
+export default RecipePage;
