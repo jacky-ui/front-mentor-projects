7a480aaf448fe18f5f98ab8419a1f95b3a262b69
jacky-ui <jacky.du.004@gmail.com>
2024-11-07 16:02:36 -0500

    create page for dashboard

---
+++ src/pages/TimeDashboardPage/TimeDashboardPage.js
@@ -0,0 +1,11 @@
+import './TimeDashboardPage.scss';
+
+function TimeDashboardPage() {
+    return(
+        <>
+            <h1>Page</h1>
+        </>
+    );
+};
+
+export default TimeDashboardPage;
