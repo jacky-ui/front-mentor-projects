38edc6613a0b1050ed3f997bf3dc48dd7afd37c0
jacky-ui <jacky.du.004@gmail.com>
2024-11-11 15:56:02 -0500

    desktop design of main looks good

--- src/pages/TimeDashboardPage/TimeDashboardPage.js
+++ src/pages/TimeDashboardPage/TimeDashboardPage.js
@@ -1,5 +1,6 @@
 import setBodyClass from '../../utils/SetBodyClass';
 import setTitle from '../../utils/SetTitle';
+import avatarProfile from '../../assets/images/image-jeremy.png';
 import './TimeDashboardPage.scss';
 
 function TimeDashboardPage() {
@@ -7,7 +8,19 @@ function TimeDashboardPage() {
     setTitle('Frontend Mentor | Time Dashboard');
     return(
         <main>
-            <h1>Page</h1>
+            <section className='dashboard'>
+                <article className='dashboard__mainCard'>
+                    <div className='top__card'>
+                        <img src={avatarProfile} alt=''/>
+                        <h1><span>Report for</span><br aria-hidden='true'/> Jeremy Robson</h1>
+                    </div>
+                    <div className='bottom__card'>
+                        <button className='notActive card__title'>Daily</button>
+                        <button className='active card__title'>Weekly</button>
+                        <button className='notActive card__title'>Monthly</button>
+                    </div>
+                </article>
+            </section>
         </main>
     );
 };
