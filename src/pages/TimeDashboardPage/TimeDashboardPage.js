99d4762bdafd48a4a6a099a8f85ff2106c38e878
jacky-ui <jacky.du.004@gmail.com>
2024-11-16 13:45:37 -0500

    commit before checkout

--- src/pages/TimeDashboardPage/TimeDashboardPage.js
+++ src/pages/TimeDashboardPage/TimeDashboardPage.js
@@ -1,6 +1,8 @@
 import setBodyClass from '../../utils/SetBodyClass';
 import setTitle from '../../utils/SetTitle';
 import avatarProfile from '../../assets/images/image-jeremy.png';
+import ellipsis from '../../assets/images/icon-ellipsis.svg';
+import work from '../../assets/images/icon-work.svg';
 import './TimeDashboardPage.scss';
 
 function TimeDashboardPage() {
@@ -15,11 +17,115 @@ function TimeDashboardPage() {
                         <h1><span>Report for</span><br aria-hidden='true'/> Jeremy Robson</h1>
                     </div>
                     <div className='bottom__card'>
-                        <button className='notActive card__title'>Daily</button>
-                        <button className='active card__title'>Weekly</button>
-                        <button className='notActive card__title'>Monthly</button>
+                        <button className='notActive card__title' type='button' aria-label='Click here to view daily numbers'>Daily</button>
+                        <button className='active card__title' type='button' aria-label='Click here to view weekly numbers'>Weekly</button>
+                        <button className='notActive card__title' type='button' aria-label='Click here to view monthly numbers'>Monthly</button>
                     </div>
                 </article>
+                <section>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className='dashboard__secondaryCard'>
+                    <div className='top'>
+                        <img  src={work} alt=''/>
+                    </div>
+                    <div className='bottom'>
+                        <div>
+                            <div className='dashboard__secondaryCard--title'>
+                                <h2 className='card__title'>Work</h2>
+                                <img src={ellipsis} alt=''/>
+                            </div>
+                            <div className='dashboard__secondaryCard--content'>
+                                <span>32hrs</span>
+                                <span>Last Week - 36hrs</span>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                </section>
             </section>
         </main>
     );
