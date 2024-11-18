97eb76ea7bd2d9846fecce5100223a957248a9ab
jacky-ui <jacky.du.004@gmail.com>
2024-11-18 18:10:23 -0500

    import search bar

--- src/pages/IpAddressPage/IpAddressPage.js
+++ src/pages/IpAddressPage/IpAddressPage.js
@@ -1,6 +1,10 @@
+import useSetBodyClass from '../../utils/SetBodyClass';
+import useSetTitle from '../../utils/SetTitle';
 import './IpAddressPage.scss';
 
 function IpAddressPage() {
+    useSetBodyClass('ipBody');
+    useSetTitle('Frontend Mentor | IP Address Tracker');
     return(
         <>
             <h1>Helo World</h1>
