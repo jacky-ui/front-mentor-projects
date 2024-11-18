3660755426df39308021d14688e8114aba293613
jacky-ui <jacky.du.004@gmail.com>
2024-11-18 18:14:12 -0500

    import header and change variable names

---
+++ src/components/IpHeader/IpHeader.js
@@ -0,0 +1,13 @@
+import SearchBar from '../SearchBar/SearchBar.js';
+import './Header.scss';
+
+function Header() {
+    return(
+        <header className='header'>
+            <h1 className='header__title'>IP Address Tracker</h1>
+            <SearchBar />
+        </header>
+    )
+};
+
+export default Header;
